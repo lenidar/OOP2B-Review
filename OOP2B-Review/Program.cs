d8a92a4cb3526665d9388b9913139e5f17d7e6ff
Julian Laspona <JulianDevAcc@hotmail.ph>
2025-09-04 10:33:37 +0800

    Add project files.

---
+++ OOP2B-Review/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OOP2B_Review
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+
+            //int c = AddNums(a, b);
+            //Console.WriteLine(AddNums(c, int.Parse(Console.ReadLine())));
+
+            MultNum(5, 4);
+            DivNum(4, 9);
+
+            Console.ReadKey();
+        }
+
+        static int AddNums(int x, int y)
+        {
+            //Console.WriteLine($"The sum of {x} and {y} is {x + y}");
+            return x + y;
+        }
+
+        static int SubNums(int x, int y)
+        {
+            return x - y;
+        }
+
+        static void MultNum(int x, int y)
+        {
+            int prod = 0;
+            for(int i = 0; i < y;  i++)
+            {
+                prod = AddNums(prod, x);
+            }
+
+            Console.WriteLine($"{x} * {y} = {prod}");
+        }
+
+        static void DivNum(int x, int y)
+        {
+            int quo = 0;
+
+            while(x > y)
+            {
+                x = SubNums(x, y);
+            }
+
+            Console.WriteLine($"{x} / {y} = {quo} r {x}");
+        }
+    }
+}
